841620594226bdd510805747786bfce69e4ecde8
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-02-17 20:55:01 -0500

    Fixed PrizePercentage property.

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -11,6 +11,6 @@ namespace TrackerLibrary
         public int PlaceNumber { get; set; }
         public string PlaceName { get; set; }
         public decimal PrizeAmount { get; set; }
-        public double P { get; set; }
+        public double PrizePercentage { get; set; }
     }
 }
