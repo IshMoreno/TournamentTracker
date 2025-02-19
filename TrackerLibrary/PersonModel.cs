6a091666001ffa89ed79ea03c672e34e003888f9
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-02-18 21:58:11 -0500

    Added additional XML comments to the rest of the classes.

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person.
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
         public int FirstName { get; set; }
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
         public int LastName { get; set; }
+        /// <summary>
+        /// The primary email address of the person.
+        /// </summary>
         public string EmailAddress { get; set; } 
+        /// <summary>
+        /// the primary cell phone number of the person.
+        /// </summary>
         public string CellphoneNumber { get; set; }
     }
 }
--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents a team participating in the tournament.
+    /// </summary>
     public class TeamModel
     {
+        /// <summary>
+        /// The list of team member in this team.
+        /// </summary>
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        /// <summary>
+        /// The name of the team.
+        /// </summary>
         public string TeamName { get; set; }
     }
 }
