3cff892e1d9fce7cda301f911ffa6500da8c15bf
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-02-27 22:43:30 -0500

    Started the Create Team form part 1.

---
+++ TrackerUI/CreateTeamForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TrackerUI
+{
+    partial class CreateTeamForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateTeamForm));
+            teamNameValue = new TextBox();
+            teamNameLabel = new Label();
+            headerLabel = new Label();
+            SuspendLayout();
+            // 
+            // teamNameValue
+            // 
+            teamNameValue.Location = new Point(43, 150);
+            teamNameValue.Name = "teamNameValue";
+            teamNameValue.Size = new Size(347, 35);
+            teamNameValue.TabIndex = 13;
+            // 
+            // teamNameLabel
+            // 
+            teamNameLabel.AutoSize = true;
+            teamNameLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            teamNameLabel.ForeColor = Color.FromArgb(51, 153, 255);
+            teamNameLabel.Location = new Point(33, 110);
+            teamNameLabel.Name = "teamNameLabel";
+            teamNameLabel.Size = new Size(157, 37);
+            teamNameLabel.TabIndex = 12;
+            teamNameLabel.Text = "Team Name";
+            // 
+            // headerLabel
+            // 
+            headerLabel.AutoSize = true;
+            headerLabel.Font = new Font("Segoe UI Light", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            headerLabel.ForeColor = Color.FromArgb(51, 153, 255);
+            headerLabel.Location = new Point(33, 30);
+            headerLabel.Name = "headerLabel";
+            headerLabel.Size = new Size(213, 50);
+            headerLabel.TabIndex = 11;
+            headerLabel.Text = "Create Team";
+            // 
+            // CreateTeamForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1371, 900);
+            Controls.Add(teamNameValue);
+            Controls.Add(teamNameLabel);
+            Controls.Add(headerLabel);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "CreateTeamForm";
+            Text = "Create Team";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox teamNameValue;
+        private Label teamNameLabel;
+        private Label headerLabel;
+    }
+}
